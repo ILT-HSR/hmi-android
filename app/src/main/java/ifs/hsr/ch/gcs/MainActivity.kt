b946ce9ba813728998c5c30e7fc8d97d39e07aca
Felix Morgner <fmorgner@hsr.ch>
2018-04-06 18:40:03 +0200

    map: add OSM map view to main activity

--- app/src/main/java/ifs/hsr/ch/gcs/MainActivity.kt
+++ app/src/main/java/ifs/hsr/ch/gcs/MainActivity.kt
@@ -2,11 +2,33 @@ package ifs.hsr.ch.gcs
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.views.MapView
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Configuration.getInstance().load(applicationContext, PreferenceManager.getDefaultSharedPreferences(applicationContext))
+
         setContentView(R.layout.activity_main)
+
+        mapView = findViewById<MapView>(R.id.map)
+        mapView.setTileSource(TileSourceFactory.MAPNIK)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
     }
 }
