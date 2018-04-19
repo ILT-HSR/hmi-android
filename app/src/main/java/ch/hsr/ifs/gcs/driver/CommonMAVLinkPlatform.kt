8297079c27dddfe2904d6146412c569d336255e1
Felix Morgner <fmorgner@hsr.ch>
2018-04-19 17:08:42 +0200

    libs: update jMAVlib

--- app/src/main/java/ch/hsr/ifs/gcs/driver/CommonMAVLinkPlatform.kt
+++ app/src/main/java/ch/hsr/ifs/gcs/driver/CommonMAVLinkPlatform.kt
@@ -1,10 +1,14 @@
 package ch.hsr.ifs.gcs.driver
 
+import me.drton.jmavlib.mavlink.MAVLinkSchemaRegistry
+
 /**
  * This driver interface specifies the commands and queries support by 'Common' profile MAVLink
  * vehicles.
  */
 interface CommonMAVLinkPlatform : Platform {
+
+    val schema get() = MAVLinkSchemaRegistry["common"]
 
     /**
      * Arm the vehicle for takeoff
