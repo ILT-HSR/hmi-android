fb7e1bb2ced02426e692655164dd52b964c5e75e
Mario Meili <mario.meili@live.com>
2018-05-17 11:05:54 +0200

    needs: extended documentation

--- app/src/main/java/ch/hsr/ifs/gcs/needs/Need.kt
+++ app/src/main/java/ch/hsr/ifs/gcs/needs/Need.kt
@@ -12,22 +12,30 @@ interface Need {
 
     /**
      * The name of the need. This is shown in the user interface.
+     *
+     * @since 1.0.0
      */
     val name: String
 
     /**
      * The list of tasks necessary to fulfill the need. This is needed for the recycler view.
+     *
+     * @since 1.0.0
      */
     val needParameterList: List<NeedParameter<*>>
 
     /**
      * A need is set to active, if it is selected via touch or button navigation.
+     *
+     * @since 1.0.0
      */
     var isActive: Boolean
 
     /**
      * Translate the need into a list of [Task] instances.
      * @return A list of [Task] instances or null, if the need parameters are not completed.
+     *
+     * @since 1.0.0
      */
     fun getTasks(): List<Task>?
 
--- app/src/main/java/ch/hsr/ifs/gcs/needs/NeedParameter.kt
+++ app/src/main/java/ch/hsr/ifs/gcs/needs/NeedParameter.kt
@@ -9,43 +9,59 @@ interface NeedParameter<Result> {
 
     /**
      * The name of the task. This is shown in the user interface.
+     *
+     * @since 1.0.0
      */
     val name: String
 
     /**
      * A short description of what the user needs to do to complete this task.
+     *
+     * @since 1.0.0
      */
     val description: String
 
     /**
      * The result of the task when completed.
+     *
+     * @since 1.0.0
      */
     var result: Result?
 
     /**
      * Provides a [String] representation of the result. This is shown in the user interface.
+     *
+     * @since 1.0.0
      */
     fun resultToString(): String
 
     /**
      * A task is set to active, if previous task are completed, and the current task is not.
+     *
+     * @since 1.0.0
      */
     var isActive: Boolean
 
     /**
      * A task is set to completed, if a result has been computed.
+     *
+     * @since 1.0.0
      */
     var isCompleted: Boolean
 
     /**
      * The setup function prepares the ui for upcoming user interaction
      * @param context The main activity
+     *
+     * @since 1.0.0
      */
     fun setup(context: MainActivity)
 
     /**
      * The cleanup function cleans up the ui after successful user interaction
      * @param context The main activity
+     *
+     * @since 1.0.0
      */
     fun cleanup(context: MainActivity)
 
