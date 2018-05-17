68abb6224a7dd748f83503891206a42dc039f0de
Mario Meili <mario.meili@live.com>
2018-05-17 09:42:42 +0200

    model: extend task interface with executeOn function

--- app/src/main/java/ch/hsr/ifs/gcs/model/Task.kt
+++ app/src/main/java/ch/hsr/ifs/gcs/model/Task.kt
@@ -1,7 +1,17 @@
 package ch.hsr.ifs.gcs.model
 
+/**
+ * This task interface specifies the basic tasks that vehicles are capable of on an abstract level.
+ *
+ * @since 1.0.0
+ * @author IFS Institute for Software
+ */
 interface Task {
 
-
+    /**
+     * Execute this task with the given [resource].
+     * @param resource The resource with which the task should be executed.
+     */
+    fun executeOn(/*TODO: Add resource parameter*/)
 
 }
