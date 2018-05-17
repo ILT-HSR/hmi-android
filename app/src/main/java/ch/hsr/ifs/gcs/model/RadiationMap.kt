45a8e7b5472ef2ab2e8fb351c1775566a48675fb
Mario Meili <mario.meili@live.com>
2018-05-17 09:47:31 +0200

    model: refactor and document radiation map need

--- app/src/main/java/ch/hsr/ifs/gcs/model/RadiationMap.kt
+++ app/src/main/java/ch/hsr/ifs/gcs/model/RadiationMap.kt
@@ -1,12 +1,19 @@
 package ch.hsr.ifs.gcs.model
 
+/**
+ * This [Need] implementation represents the need to generate a heat map for radiation in a
+ * provided region using a certain mode.
+ *
+ * @since 1.0.0
+ * @author IFS Institute for Software
+ */
 class RadiationMap : Need {
 
     private val regionParameter = ChooseRegionNeedParameter()
     private val altitudeParameter = ChooseAltitudeNeedParameter()
     private val modeParameter = ChooseModeNeedParameter()
 
-    override val name get() = "Radiation Map"
+    override val name = "Radiation Map"
 
     override val needParameterList: List<NeedParameter<Any>> = arrayListOf(
             regionParameter as NeedParameter<Any>,
