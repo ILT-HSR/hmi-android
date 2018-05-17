144bb585d4ba83e955585af542633dc67c03d871
Mario Meili <mario.meili@live.com>
2018-05-17 09:45:44 +0200

    model: refactor and document callin need

--- app/src/main/java/ch/hsr/ifs/gcs/model/CallIn.kt
+++ app/src/main/java/ch/hsr/ifs/gcs/model/CallIn.kt
@@ -1,11 +1,17 @@
 package ch.hsr.ifs.gcs.model
 
+/**
+ * This [Need] implementation represents the need to drop a cargo at a chosen location.
+ *
+ * @since 1.0.0
+ * @author IFS Institute for Software
+ */
 class CallIn : Need {
 
     private val targetParameter = ChooseTargetNeedParameter()
     private val cargoParameter = ChooseCargoNeedParameter()
 
-    override val name get() = "Call-in"
+    override val name = "Call-in"
 
     override val needParameterList: List<NeedParameter<Any>> = arrayListOf(
             targetParameter as NeedParameter<Any>,
