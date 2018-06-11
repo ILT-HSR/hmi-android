888d98ed3fcd175447d742834598d53aa63bc208
Felix Morgner <fmorgner@hsr.ch>
2018-06-11 15:47:31 +0200

    mission: code cleanup

--- app/src/main/java/ch/hsr/ifs/gcs/mission/Mission.kt
+++ app/src/main/java/ch/hsr/ifs/gcs/mission/Mission.kt
@@ -2,6 +2,7 @@ package ch.hsr.ifs.gcs.mission
 
 import android.util.Log
 import ch.hsr.ifs.gcs.needs.Need
+import ch.hsr.ifs.gcs.resources.Resource
 import java.util.concurrent.Executors
 
 /**
@@ -11,11 +12,9 @@ import java.util.concurrent.Executors
  * @since 1.0.0
  * @author IFS Institute for Software
  */
-open class Mission(private val fNeed: Need) {
+class Mission(private val fResource: Resource, private val fNeed: Need) {
 
     private val LOG_TAG = Mission::class.simpleName
-
-    private val fMissionExecutor = Executors.newSingleThreadScheduledExecutor()
 
     init {
         Log.i(LOG_TAG, "Created new mission with the following tasks: ${fNeed.getTasks()}")
