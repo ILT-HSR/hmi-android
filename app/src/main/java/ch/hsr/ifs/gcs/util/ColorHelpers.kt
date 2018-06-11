4e1be17e783f5d0f8098c7d59b91a2abf9d01a10
Felix Morgner <fmorgner@hsr.ch>
2018-06-11 16:48:24 +0200

    util: simplify createRandomColorArgb

--- app/src/main/java/ch/hsr/ifs/gcs/util/ColorHelpers.kt
+++ app/src/main/java/ch/hsr/ifs/gcs/util/ColorHelpers.kt
@@ -3,11 +3,10 @@ package ch.hsr.ifs.gcs.util
 import android.graphics.Color
 import java.util.*
 
-fun createRandomColorArgb() : Int {
-    val random = Random()
-    fun rand(from: Int, to: Int) : Int {
-        return random.nextInt(to - from) + from
-    }
-    val color = Color.argb(255, rand(0, 255), rand(0, 255), rand(0, 255))
-    return color
-}
+fun createRandomColorArgb() =
+        with(Random()) {
+            fun rand(from: Int, to: Int): Int {
+                return nextInt(to - from) + from
+            }
+            Color.argb(255, rand(0, 255), rand(0, 255), rand(0, 255))
+        }
