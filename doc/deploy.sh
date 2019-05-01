38a3f2e2e0670a058933c2f55b0445a7ac2236e7
Felix Morgner <fmorgner@hsr.ch>
2019-05-01 12:55:26 +0200

    build: fix deploy key access

--- doc/deploy.sh
+++ doc/deploy.sh
@@ -33,12 +33,12 @@ git add -A .
 git commit -m "doc: update documentation for ${TRAVIS_BRANCH}@${SHA}"
 
 echo "Decrypting deployment key"
-openssl aes-256-cbc -K $encrypted_a7ed42facd4e_key -iv $encrypted_a7ed42facd4e_iv -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $encrypted_a7ed42facd4e_key -iv $encrypted_a7ed42facd4e_iv -in ../deploy_key.enc -out ../deploy_key -d
 
 echo "Initializing ssh-agent with deployment key"
-chmod 600 deploy_key
+chmod 600 ../deploy_key
 eval $(ssh-agent -s)
-ssh-add deploy_key
+ssh-add ../deploy_key
 
 echo "Publishing changes"
 REPO=$(git config remote.origin.url)
