6b6787510f53325b47375a01d3f09d6ac716f516
Mario Stäubli <mario.staeubli@outlook.com>
2019-05-02 10:49:35 +0200

    app: add preference constant keys

--- app/src/main/kotlin/ch/hsr/ifs/gcs/PreferenceConstants.kt
+++ app/src/main/kotlin/ch/hsr/ifs/gcs/PreferenceConstants.kt
@@ -1,7 +1,13 @@
 package ch.hsr.ifs.gcs
 
 const val PREFERENCE_KEY_ENABLE_NULL_PLATFORM = "enable_null_platform"
-const val PREFERENCE_KEY_MAP_SOURCE = "map_source"
 
+const val PREFERENCE_KEY_MAP_SOURCE = "map_source"
 const val PREFERENCE_VAL_MAP_SOURCE_BING = "bing"
 const val PREFERENCE_VAL_MAP_SOURCE_OSM = "osm"
+
+const val PREFERENCE_KEY_CALL_IN_ALTITUDE = "call_in_altitude"
+const val PREFERENCE_KEY_CALL_IN_TRAVEL_SPEED = "call_in_travel_speed"
+
+const val PREFERENCE_KEY_MAPPING_ALTITUDE = "mapping_altitude"
+const val PREFERENCE_KEY_MAPPING_TRAVEL_SPEED = "mapping_travel_speed"
