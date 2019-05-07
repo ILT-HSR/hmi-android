7478928302efd143a5acdc836ce58d209fba862d
Mario Stäubli <mario.staeubli@outlook.com>
2019-05-07 09:58:13 +0200

    ui: add helper for setting and hiding instruction text

--- app/src/main/kotlin/ch/hsr/ifs/gcs/ui/mission/need/parameter/ParameterConfigurator.kt
+++ app/src/main/kotlin/ch/hsr/ifs/gcs/ui/mission/need/parameter/ParameterConfigurator.kt
@@ -1,6 +1,9 @@
 package ch.hsr.ifs.gcs.ui.mission.need.parameter
 
 import android.support.v4.app.Fragment
+import android.view.View
+import android.widget.TextView
+import ch.hsr.ifs.gcs.R
 import ch.hsr.ifs.gcs.ui.MainActivity
 
 abstract class ParameterConfigurator<ResultType> : Fragment() {
@@ -14,5 +17,17 @@ abstract class ParameterConfigurator<ResultType> : Fragment() {
     open fun destroy() = Unit
 
     open fun abort() = destroy()
+
+    fun showInstructionText(instructionText: String) {
+        val parameterInstructionText = context.findViewById<TextView>(R.id.parameterInstructionText)
+        parameterInstructionText.text = instructionText
+        parameterInstructionText.visibility = View.VISIBLE
+    }
+
+    fun hideInstructionText() {
+        val mapInstructionText = context.findViewById<TextView>(R.id.parameterInstructionText)
+        mapInstructionText.text = ""
+        mapInstructionText.visibility = View.INVISIBLE
+    }
 
 }
