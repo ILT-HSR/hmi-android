706252bd25aadd7f14960ed7fed57635001905f0
Felix Morgner <fmorgner@hsr.ch>
2019-10-07 06:20:27 +0200

    driver: automatically detect remote UDP endpoint

--- app/src/main/kotlin/ch/hsr/ifs/gcs/driver/channel/UdpDataChannel.kt
+++ app/src/main/kotlin/ch/hsr/ifs/gcs/driver/channel/UdpDataChannel.kt
@@ -3,13 +3,15 @@ package ch.hsr.ifs.gcs.driver.channel
 import java.net.Inet4Address
 import java.net.InetSocketAddress
 import java.net.StandardProtocolFamily.INET
+import java.nio.ByteBuffer
 import java.nio.channels.ByteChannel
 import java.nio.channels.DatagramChannel
 
 private fun makeChannel(port: Int) = DatagramChannel.open(INET).apply {
     bind(InetSocketAddress(Inet4Address.getByName("0.0.0.0"), port))
-    connect(InetSocketAddress(Inet4Address.getByName("10.0.2.2"), 14570))
-    configureBlocking(false)
+    configureBlocking(true)
+    val remote = receive(ByteBuffer.wrap(ByteArray(256)))
+    connect(remote)
 }
 
 class UdpDataChannel(port: Int) : ByteChannel by makeChannel(port)
