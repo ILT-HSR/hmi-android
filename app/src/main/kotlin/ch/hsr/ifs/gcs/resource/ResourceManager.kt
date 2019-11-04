ae450af8f8578d0468f105e65c8c9f62834dd56c
Felix Morgner <fmorgner@hsr.ch>
2019-11-04 14:54:31 +0100

    resource: extract 'ResourceDescriptor'

--- app/src/main/kotlin/ch/hsr/ifs/gcs/resource/ResourceManager.kt
+++ app/src/main/kotlin/ch/hsr/ifs/gcs/resource/ResourceManager.kt
@@ -9,14 +9,8 @@ import ch.hsr.ifs.gcs.driver.PlatformDrivers
 import ch.hsr.ifs.gcs.driver.PayloadDrivers
 import ch.hsr.ifs.gcs.driver.channel.SerialDataChannelFactory
 import ch.hsr.ifs.gcs.driver.channel.UdpDataChannelFactory
-import com.google.gson.JsonElement
-import com.google.gson.JsonObject
-import com.google.gson.JsonParser
 import com.hoho.android.usbserial.driver.UsbSerialDriver
 import com.hoho.android.usbserial.driver.UsbSerialProber
-import java.io.InputStream
-import java.io.InputStreamReader
-import java.io.Serializable
 import java.nio.channels.ByteChannel
 import java.util.concurrent.Executors
 
@@ -26,51 +20,6 @@ class ResourceManager(private val fListener: Listener, private val fChannelType:
         fun onNewResourceAvailable(resource: Resource)
         fun onResourceAcquired(resource: Resource)
         fun onResourceUnavailable(resource: Resource)
-    }
-
-    data class ResourceDescriptor(val id: String, val driver: String, val payloadDrivers: List<String>, val capabilities: List<Capability<*>>) {
-        companion object {
-            fun load(stream: InputStream): ResourceDescriptor =
-                    JsonParser().parse(InputStreamReader(stream, Charsets.UTF_8)).asJsonObject.let { res ->
-                        val id = res["id"].asString
-                        val capabilities = loadCapabilities(res)
-                        val platformDescriptor = res["platform"].asJsonObject
-                        val payloadDrivers = loadPayloadDrivers(res)
-                        ResourceDescriptor(
-                                id,
-                                platformDescriptor["driver"].asString,
-                                payloadDrivers,
-                                capabilities
-                        )
-                    }
-
-            private fun loadPayloadDrivers(res: JsonObject) =
-                    if (res.has("payloads")) {
-                        res["payloads"].asJsonArray
-                                .asSequence()
-                                .map(JsonElement::getAsJsonObject)
-                                .mapNotNull { it["driver"].asString }
-                                .toList()
-                    } else {
-                        emptyList()
-                    }
-
-            private fun loadCapabilities(res: JsonObject): List<Capability<out Serializable>> {
-                return res["capabilities"].asJsonArray
-                        .asSequence()
-                        .map(JsonElement::getAsJsonObject)
-                        .mapNotNull { obj ->
-                            BUILTIN_CAPABILITIES[obj["id"].asString]?.let { cap ->
-                                when (cap.type) {
-                                    "boolean" -> Capability(cap, obj["value"].asBoolean)
-                                    "number" -> Capability(cap, obj["value"].asNumber)
-                                    else -> null
-                                }
-                            }
-                        }
-                        .toList()
-            }
-        }
     }
 
     companion object {
